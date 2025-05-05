c7012f66c3f2d266fe06a6c730f873d8844a111c
Eddy-Ruphay <hivox.contacto@gmail.com>
2025-05-05 17:34:56 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,23 @@
 module.exports = {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
-  theme: { extend: {} },
-  plugins: [],
+  content: [
+    './index.html',
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#4f46e5', // Roxo-vibrante Hivox
+        background: '#f9fafb',
+        texto: '#111827',
+      },
+      fontFamily: {
+        sans: ['Roboto', 'Ubuntu', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+  ],
 };
