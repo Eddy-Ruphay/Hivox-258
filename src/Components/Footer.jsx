5e077f0b238b04679859723929b0b3c471654014
Eddy-Ruphay <hivox.contacto@gmail.com>
2025-05-05 08:37:11 +0200

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,9 @@
+import React from 'react';
+
 export default function Footer() {
   return (
-    <footer className="text-center text-xs text-gray-500 p-4">
-      © 2025 Hivox. Todos os direitos reservados.
+    <footer className="bg-gray-200 text-center py-4 mt-8 text-sm text-gray-600">
+      &copy; {new Date().getFullYear()} Hivox. Cresce ouvindo.
     </footer>
   );
 }
