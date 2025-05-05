aec29363d3ad8246b3d3dde15728d42b79428866
Eddy-Ruphay <hivox.contacto@gmail.com>
2025-05-05 08:31:56 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
+import Home from './pages/Home';
 
 function App() {
-  return (
-    <div className="min-h-screen bg-gray-100 p-6">
-      <h1 className="text-3xl font-bold text-center text-indigo-600">Hivox – Cresce ouvindo</h1>
-      <p className="text-center mt-2 text-gray-700">Seu universo de audiolivros em um só lugar.</p>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
